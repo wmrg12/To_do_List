ec58b50a67d2d90511a1266272b868f2ac5490f2
Vicente Guzman Aliza Abigail <alizaabigailvicenteguzman@gmail.com>
2026-06-05 09:15:07 -0400

    feat(modelos): agregar campo token al esquema de usuario

--- backend_express/models/user.js
+++ backend_express/models/user.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    token: { type: String }
 });
 
 module.exports = mongoose.model('User', UserSchema);
