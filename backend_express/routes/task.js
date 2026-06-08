344f7b3b5ff0b9b48d4e94383a70d6c572a7c2a1
Adriana Soto <soto7adriana@gmail.com>
2026-06-07 23:01:32 -0400

    refactor(task): separar archivos de tareas

--- backend_express/routes/task.js
+++ backend_express/routes/task.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const taskController = require('../controllers/taskController');
 const verifyToken = require('../middleware/verifyToken');
-const upload = require('../middleware/upload');
 
 router.use(verifyToken);
 
@@ -11,12 +10,6 @@ router.get('/', taskController.task_list);
 
 // POST /task -> crea uno
 router.post('/', taskController.task_create);
-
-// GET /task/:id/download -> descarga el archivo asociado a la tarea
-router.get('/:id/download', taskController.task_download);
-
-// DELETE /task/:id/file -> elimina el archivo asociado a la tarea
-router.delete('/:id/file', taskController.task_delete_file);
 
 // GET /task/:id -> obtiene uno
 router.get('/:id', taskController.task_detail);
@@ -29,7 +22,5 @@ router.patch('/:id', taskController.task_patch);
 
 // DELETE /task/:id -> elimina uno
 router.delete('/:id', taskController.task_delete);
-
-router.post('/:id/upload', upload.single('file'), taskController.task_upload);
 
 module.exports = router;
